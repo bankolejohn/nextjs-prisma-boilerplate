2878219481be38d103be61abad2df0f65a9be3d8
bankolejohn <bankolejohn@gmail.com>
2026-06-06 04:41:54 +0100

    fix: remove duplicate ssrNcHandler and NextApiRequestWithResult in nc.ts
    fs_append accidentally duplicated the entire ssrNcHandler block causing
    TypeScript to throw: Duplicate identifier 'NextApiRequestWithResult'
    which failed the production Docker build in GitHub Actions.

--- lib-server/nc.ts
+++ lib-server/nc.ts
@@ -94,39 +94,3 @@ export const ssrNcHandler = async <T extends unknown>(
 };
 
 export default nc;
-
-// ---------- getServerSideProps error handler
-
-export type NextApiRequestWithResult<T> = NextApiRequest & { result: T };
-
-/* eslint-disable @typescript-eslint/no-unnecessary-type-constraint */
-export const ssrNcHandler = async <T extends unknown>(
-  req: NextReq,
-  res: ServerResponse,
-  callback: () => Promise<T>
-) => {
-  const base = () => {
-    const handler = nc<NextApiRequestWithResult<T>, NextApiResponse>(ncOptions).use(
-      async (req, res, next) => {
-        req.result = await callback();
-        next();
-      }
-    );
-
-    return handler;
-  };
-
-  const _req = req as NextApiRequestWithResult<T>;
-  const _res = res as NextApiResponse;
-
-  try {
-    await base().run(_req, _res);
-
-    return _req.result as T;
-  } catch (error) {
-    handleSsrError(error, _req, _res);
-    return null;
-  }
-};
-
-export default nc;
